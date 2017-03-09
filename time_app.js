f1292490441413080c08c6922e449d7a34e045ad
danny <dannypsnl@gmail.com>
2017-03-10 02:12:36 +0800

    Format the output and show each worker's data

--- time_app.js
+++ time_app.js
@@ -12,12 +12,13 @@ var TotalUseTime = 0,
 for (var i in workers) {
   var worker = workers[i];
   total_waiting_time += worker.total_wait;
+  console.log("Worker "+i+" cost "+worker.total_wait+" mins");
   if(worker.cost_time > TotalUseTime) {
     TotalUseTime = worker.cost_time;
   }
 }
-console.log(total_waiting_time);
+console.log("Total waiting time: "+total_waiting_time+" mins");
 for (var i in workers) {
   var worker = workers[i];
-  console.log(worker.cost_time/TotalUseTime*100+"%");
+  console.log("Worker "+i+" use rate: "+worker.cost_time/TotalUseTime*100+" %");
 }
