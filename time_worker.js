71cf822697cb7ee4451eff67ed57df3d2a0790be
danny <dannypsnl@gmail.com>
2017-03-10 01:24:15 +0800

    Worker have MAX waiting queue now

--- time_worker.js
+++ time_worker.js
@@ -1,28 +1,50 @@
 const time_patients = require('./time_patient.js');
 
+var Worker = {
+  Patients: [],
+  Max: 50,
+  hasRoom: function() {
+    return this.Patients.length < this.Max
+  }
+};
+
 Patients = []
 
 for(let i=0; i<1000; i+=5) {
   Patients.push(new time_patients.Patient(i));
 }
 
+var amount_of_Patients = 200;
 var now = 0;
-
-for (var i in Patients) {
-  var Patient = Patients[i]
-  if(Patient.arriveTime > now) {
-    now = Patient.arriveTime;
-  } else {
-    Patient.waitingTime = now - Patient.arriveTime;
-  }
-  Patient.dealTime = 1;
-  now += Patient.dealTime
-}
-
 var total_waiting_time = 0;
-for (var i in Patients) {
-  Patient = Patients[i];
-  total_waiting_time += Patient.waitingTime;
+
+while(true) {
+  var _Patient = []
+  if(_Patient.length === 0) {
+    _Patient.push(Patients.shift())
+    amount_of_Patients -= 1;
+  }
+  if(Worker.hasRoom()) {
+    Worker.Patients.push(_Patient.shift())
+  } else {
+    _Patient[0].arriveTime = now
+  }
+  if(Worker.Patients.length > 0) {
+    var Patient = Worker.Patients.shift()
+    if(Patient.arriveTime > now) {
+      now = Patient.arriveTime;
+    } else {
+      Patient.waitingTime = now - Patient.arriveTime;
+      total_waiting_time += Patient.waitingTime
+    }
+    Patient.dealTime = 6;
+    now += Patient.dealTime;
+    // Patients.push(Patient);
+  }
+  if(amount_of_Patients == 0) {
+    break;
+  }
+  // console.log(total_waiting_time);
 }
 
 console.log(total_waiting_time);
